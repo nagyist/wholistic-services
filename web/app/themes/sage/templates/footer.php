9101972882ee622d91447126318c47d6f32ff6de
Simon Cossar <scossar3@gmail.com>
2015-04-18 22:23:49 -0700

    set up footer

--- web/app/themes/sage/templates/footer.php
+++ web/app/themes/sage/templates/footer.php
@@ -1,5 +1,26 @@
+<?php
+use Testeleven\Positioned\TemplateTags;
+?>
+
 <footer class="content-info" role="contentinfo">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+	  <div class="row">
+		  <div class="footer-1">
+			 <?php TemplateTags\post_in_position('footer-who', 'positioned_full', 3); ?>
+		  </div>
+		  <div class="footer-2">
+			  <?php TemplateTags\post_in_position('footer-where', 'positioned_full', 3); ?>
+		  </div>
+		  <div class="footer-3">
+			  <?php TemplateTags\post_in_position('footer-site-info', 'positioned_full', 3); ?>
+			  <?php dynamic_sidebar('sidebar-footer'); ?>
+			  <?php if (has_nav_menu('footer_navigation')) : ?>
+				  <?php wp_nav_menu(['theme_location' => 'footer_navigation']); ?>
+			  <?php endif; ?>
+		  </div>
+		  <div class="testeleven">
+			  &copy; 2014-<?php echo date("Y"); ?> testeleven web development
+		  </div>
+	  </div>
   </div>
 </footer>
