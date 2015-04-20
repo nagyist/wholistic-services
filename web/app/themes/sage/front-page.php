f8175691915fa96d54e7c2e0c8ddf4b2b99a5ef3
Simon Cossar <scossar3@gmail.com>
2015-04-20 12:59:41 -0700

    remove unnecessary content from front page

--- web/app/themes/sage/front-page.php
+++ web/app/themes/sage/front-page.php
@@ -7,13 +7,13 @@ use Testeleven\Positioned\TemplateTags;
 </section>
 
 <section class="welcome" id="welcome">
-  <div class="full-width-heading">
-    <?php TemplateTags\post_in_position('welcome-to-nbs', 'positioned_title', 2); ?>
-  </div>
-  <div class="front-page-centered intro">
-    <?php TemplateTags\post_in_position('fifteen-second-intro', 'positioned_full', 3); ?>
-    <?php TemplateTags\post_in_position('fifteen-second-intro-image', 'positioned_image'); ?>
-  </div>
+<!--  <div class="full-width-heading">-->
+<!--    --><?php //TemplateTags\post_in_position('welcome-to-nbs', 'positioned_title', 2); ?>
+<!--  </div>-->
+<!--  <div class="front-page-centered intro">-->
+<!--    --><?php //TemplateTags\post_in_position('fifteen-second-intro', 'positioned_full', 3); ?>
+<!--    --><?php //TemplateTags\post_in_position('fifteen-second-intro-image', 'positioned_image'); ?>
+<!--  </div>-->
 
   <div class="full-width-heading">
     		<?php TemplateTags\post_in_position('what-we-offer', 'positioned_title', 2); ?>
@@ -34,9 +34,9 @@ use Testeleven\Positioned\TemplateTags;
   </div>
 </section>
 <section class="qualifications">
-  <div class="full-width-heading">
-    <?php TemplateTags\post_in_position('andrea', 'positioned_title', 2); ?>
-  </div>
+<!--  <div class="full-width-heading">-->
+<!--    --><?php //TemplateTags\post_in_position('andrea', 'positioned_title', 2); ?>
+<!--  </div>-->
   <div class="front-page-centered">
     <?php TemplateTags\post_in_position('who-we-are', 'positioned_full', 3); ?>
     <?php TemplateTags\post_in_position('andrea-image', 'positioned_image'); ?>
