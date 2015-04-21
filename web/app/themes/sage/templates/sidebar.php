967c0d5275a6b7a481e219121f2315ff403d303a
Simon Cossar <scossar3@gmail.com>
2015-04-21 11:04:52 -0700

    setup sidebar

--- web/app/themes/sage/templates/sidebar.php
+++ web/app/themes/sage/templates/sidebar.php
@@ -1,1 +1,7 @@
-<?php dynamic_sidebar('sidebar-primary'); ?>
+<?php
+use Testeleven\NamastePostTypes\TemplateTags;
+?>
+<?php TemplateTags\display_notifications(); ?>
+<?php TemplateTags\display_workshops(1, 3); ?>
+<?php TemplateTags\display_videos(); ?>
+<?php TemplateTags\display_testimonials(); ?>
