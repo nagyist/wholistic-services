fce8c4b05f2e9536a55a59b6a0020818bba40317
Simon Cossar <scossar3@gmail.com>
2015-04-21 12:49:20 -0700

    Add contact form to single posts

---
+++ web/app/themes/sage/templates/post-contact-form.php
@@ -0,0 +1,13 @@
+<div class="contact-service-and-class">
+  <header class="contact-header">
+    <?php // If the contact is about a 'service' or a 'class', give the correct heading. ?>
+    <?php if (has_category('service')) : ?>
+      <h2>Schedule an appointment</h2>
+    <?php elseif (has_category('class')) : ?>
+      <h2>Sign up</h2>
+    <?php else : ?>
+      <h2>Contact us</h2>
+    <?php endif; ?>
+  </header>
+  <?php gravity_form(1, false, false, false, '', false); ?>
+</div>
