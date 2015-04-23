8205692a22d2595168f58f383f02e602efd78edf
Simon Cossar <scossar3@gmail.com>
2015-04-23 09:17:04 -0700

    change fonts in header

--- web/app/themes/sage/templates/head.php
+++ web/app/themes/sage/templates/head.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="//use.typekit.net/vzs8osg.js"></script>
+    <script src="//use.typekit.net/hsl0eou.js"></script>
     <script>try{Typekit.load();}catch(e){}</script>
     <link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.ico" />
     <?php wp_head(); ?>
