0f5be586e9532c33351d5bc60a9a2a41ca0f6e0c
Simon Cossar <scossar3@gmail.com>
2015-04-23 12:32:59 -0700

    change 'read more' text

--- web/app/themes/sage/lib/extras.php
+++ web/app/themes/sage/lib/extras.php
@@ -28,7 +28,7 @@ add_filter('body_class', __NAMESPACE__ . '\\body_class');
  * Clean up the_excerpt()
  */
 function excerpt_more() {
-  return ' [<a href="' . get_permalink() . '">' . __('more...', 'sage') . '</a>]';
+  return ' <a href="' . get_permalink() . '">' . __('[read more...]', 'sage') . '</a>';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
 
